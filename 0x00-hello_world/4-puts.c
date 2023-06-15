af715381d6a5e3216fd6850b764481634493beee
Naoufalhdr <naoufalhdr96@gmail.com>
2023-06-15 15:02:51 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
- * Description : function that print out a quote to stout.
- */ 
+*Description : function that print out a quote to stout.
+*/ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
