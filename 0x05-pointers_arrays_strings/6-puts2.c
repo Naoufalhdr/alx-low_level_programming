60877700615df1a49a3f87657ebb51cb1bc9ed15
Naoufalhdr <naoufalhdr96@gmail.com>
2023-06-26 14:49:00 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints out every other character of a string.
+ * @str: string to be printed.
+ * Return: nothing.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
