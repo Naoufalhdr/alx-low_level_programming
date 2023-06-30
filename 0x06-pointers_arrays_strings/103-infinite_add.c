5f2398c26c223c26027a55ba39363691847b9c3d
Naoufalhdr <naoufalhdr96@gmail.com>
2023-06-30 13:51:01 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,32 +11,35 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int remain = 0, i = 0, j = 0, len_n1 = 0, len_n2 = 0, max = size_r - 1, sum;
+	int remain, i, j, len_n1, len_n2, max, sum;
+
+	len_n1 = len_n2 = remain = sum = i = j = 0;
+	max = size_r - 1;
 
 	while (n1[len_n1] != '\0')
-		len_n1++;
+		i = len_n1++;
 	while (n2[len_n2] != '\0')
-		len_n2++;
+		j = len_n2++;
 
-	while (i < len_n1 || j < len_n2)
+	while (i >= 0 || j >= 0)
 	{
-		int num1 = (i < len_n1) ? n1[len_n1 - 1 - i] - '0' : 0;
-		int num2 = (j < len_n2) ? n2[len_n2 - 1 - j] - '0' : 0;
+		int num1 = (i >= 0) ? n1[i] - '0' : 0;
+		int num2 = (j >= 0) ? n2[j] - '0' : 0;
 
 		sum = num1 + num2 + remain;
 		remain = sum / 10;
 		r[max] = '0' + (sum % 10);
-		i++;
-		j++;
+		i--;
+		j--;
 		max--;
 	}
 
 	if (remain != 0)
 		r[max] = remain + '0';
 	else
-		max++;
+		max += 1;
 
-	r[size_r - 1] = '\0';
+	r[size_r] = '\0';
 
 	if (max == 0)
 		return (0);
