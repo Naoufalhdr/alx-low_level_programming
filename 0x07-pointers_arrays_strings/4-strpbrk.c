ddfed31b5d9337ddd3f0f6b66bcf1467a41ff47a
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-03 01:26:02 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,15 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; s[i]; i++)
-        {
-                for (j = 0; accept[j]; j++)
-                {
-                        if (s[i] == accept[j])
-                                return (&s[i]);
-                }
-        }
-        return (0);
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (0);
 }
