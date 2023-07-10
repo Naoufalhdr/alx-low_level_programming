90bce552ee6983e48dba637b5d1f3fa153b76bb3
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-10 01:49:22 -0700

    fix error case: _strdup(NULL)

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * _strdup - Creates a duplicate of a string.
@@ -14,12 +14,15 @@ char *_strdup(char *str)
 	char *ptr;
 	int i, len;
 
-	len = strlen(str);
-	
+	len = 0;
+
 	if (str == NULL)
 		return (NULL);
 
-	ptr = malloc(len * sizeof(char));
+	while (str[len])
+		len++;
+
+	ptr = malloc((len + 1) * sizeof(char));
 	if (ptr != NULL)
 	{
 		for (i = 0; i < len; i++)
