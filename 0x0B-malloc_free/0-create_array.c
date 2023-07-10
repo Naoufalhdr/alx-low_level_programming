24dfb48c7570703cec3971829d3981bfe2348aed
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-10 01:12:59 -0700

    fix Segmentation fault... Check malloc's return !

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	ptr = malloc(size * sizeof(char));
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
-	return (ptr);
+	if (ptr != NULL)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
+		return (ptr);
+	}
+	else
+		return (NULL);
 }
