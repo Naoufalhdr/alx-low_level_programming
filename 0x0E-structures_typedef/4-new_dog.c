5356400fff43338aeedd7a9db0a9660e0d310de5
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-17 08:52:30 -0700

    Add a function that creates a new dog struct of type dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,12 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc((name_len + 1) * sizeof(char));
 	new_dog->owner = malloc((owner_len + 1) * sizeof(char));
 	if (new_dog->name == NULL || new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
 		return (NULL);
-	}
 
 	for (i = 0; name[i] != '\0'; i++)
 		new_dog->name[i] = name[i];
