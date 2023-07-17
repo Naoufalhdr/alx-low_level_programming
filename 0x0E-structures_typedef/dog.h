69e6eb0f2b708a20816c8310b6db654ca15fd38a
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-17 07:02:29 -0700

    add a header file with a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents information about a dog.
+ * @name: the name of he dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ *
+ * Description: this structure  defines the elements that store information
+ * about a dog. it includes the dog's name, age and the name of its owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
