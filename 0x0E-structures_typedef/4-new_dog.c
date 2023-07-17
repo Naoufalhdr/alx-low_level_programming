58e251f0fe75a9c5d70e895bc441dea76b6c3188
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-17 09:34:16 -0700

    fix error 8

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - calculates the length of a string
+ * @str: a pointer to the input string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * _strcpy - copies a string from source to destination
  * @dest: a pointer to the destination string
  * @src: a pointer to the source string
@@ -32,29 +49,27 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int name_len = 0;
-	int owner_len = 0;
 
 	if (name == NULL || owner == NULL || age < 0)
 		return (NULL);
-
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = malloc((name_len + 1) * sizeof(char));
-	new_dog->owner = malloc((owner_len + 1) * sizeof(char));
+	new_dog->name = malloc((_strlen(name) + 1) * sizeof(char));
+	new_dog->owner = malloc((_strlen(owner) + 1) * sizeof(char));
 	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
+	}
 
 	_strcpy(new_dog->name, name);
-	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
+	_strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
