d400f24cd131f8d2e4e35e52242b7eaa792d24e3
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-19 08:48:06 -0700

    fixing betty style

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,30 +1,62 @@
 #include "3-calc.h"
 
-/*Function to add two integers */
+/**
+ * op_add - add two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: an integer wich is the sum of the two numbers.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-/* Function to substract two integers */
+/**
+ * op_sub - substract two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: an integer wich is the difference of the two numbers.
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiply two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: an integer wich is the product of the two numbers.
+ */
 /* Function to multiply two integers */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-/* Function to divide two integers */
+/**
+ * op_div - divide two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: an integer wich is the quotien division of the two numbers.
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
-/* Function to calculate the modulus of two integers */
+/**
+ * op_mod - add two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer wich is the remainder obtained by dividing the two numbers
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
