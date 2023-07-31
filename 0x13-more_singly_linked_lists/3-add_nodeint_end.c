9d1859d833f874af36f879b7b46bb0d2077dd677
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-31 07:07:17 -0700

    fix betty style 'whitespace error'

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		ptr = *head;
 
 	while (ptr->next != NULL)
-		ptr = ptr->next; 
+		ptr = ptr->next;
 	ptr->next = new_node;
 
 	return (new_node);
