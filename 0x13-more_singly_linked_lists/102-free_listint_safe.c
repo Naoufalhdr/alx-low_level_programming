be3b95ee485c7a4512bbc3c4acadabcb7c9b3aee
Naoufalhdr <naoufalhdr96@gmail.com>
2023-07-31 15:55:37 -0700

    Initialize the file

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t linked list
+ * @h: a pointer to a pointer  to the head of the linked list.
+ *
+ * Return: the size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+
+	return (count);
+}
