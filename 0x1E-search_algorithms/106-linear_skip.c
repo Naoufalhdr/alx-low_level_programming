7efa697de5d0d07fd73f8b26053d337bd176364d
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-21 17:36:10 +0100

    Task 13: fix betty style

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -6,37 +6,42 @@
  * @list: A pointer to the head of the skip list to search in
  * @value: The value to search for
  *
- * Return: A pointer to the first node where value is located, or NULL if not found
+ * Return: A pointer to the first node where value is located
+ *         or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *go;
+	skiplist_t *curr;
 
 	if (list == NULL)
 		return (NULL);
 
-	go = list;
+	curr = list;
 
-	do {
-		list = go;
-		go = go->express;
-		printf("Value checked at index ");
-		printf("[%d] = [%d]\n", (int)go->index, go->n);
-	} while (go->express && go->n < value);
-
-	if (go->express == NULL)
+	/* Use express lane to find the block where the value may be */
+	while (curr->express && curr->n < value)
 	{
-		list = go;
-		while (go->next)
-			go = go->next;
+		list = curr;
+		curr = curr->express;
+		 printf("Value checked at index [%ld] = [%d]\n",
+				 curr->index, curr->n);
 	}
 
-	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
-
-	while (list != go->next)
+	/* If reached the end of express lane, traverse to the end */
+	if (curr->express == NULL)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
+		list = curr;
+		while (curr->next)
+			curr = curr->next;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			list->index, curr->index);
+
+	/* Perform linear search within the identified block */
+	while (list != curr->next)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
 		if (list->n == value)
 			return (list);
 		list = list->next;
